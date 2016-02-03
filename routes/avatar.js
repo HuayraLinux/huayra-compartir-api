08f3611eb6a0a8e0bc7332c20516c0d54be8e3b4
Hugo Ruscitti <hugoruscitti@gmail.com>
2016-02-03 12:35:51 -0300

    Reparando el envío del avatar.

--- routes/avatar.js
+++ routes/avatar.js
@@ -3,7 +3,6 @@ var express = require('express');
 var router = express.Router();
 var path = require("path");
 
-
 router.get('/', function(req, res, next) {
   var ruta_completa = path.join(process.env.HOME, ".huayra-compartir_avatar");
 
@@ -12,7 +11,19 @@ router.get('/', function(req, res, next) {
     if (exists == false) {
       res.sendFile("avatar_por_omision.png", {root: __dirname + '/../public/'});
     } else {
-      res.sendFile(".huayra-compartir_avatar", {root: process.env.HOME});
+      var filename = path.basename(ruta_completa);
+
+      var filestream = fs.createReadStream(ruta_completa);
+
+      filestream.on('error', function (error) {
+        console.error(error);
+        res.json({"Error": error})
+      });
+
+      filestream.on('readable', function() {
+        filestream.pipe(res);
+      });
+
     }
 
   });
